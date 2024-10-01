52d087e0806530c49d9c8abed8b366ee8c7e53a5
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2024-10-01 21:40:25 +1100

    Display version in AboutApp

--- src/views/AboutApp.vue
+++ src/views/AboutApp.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref } from "vue";
 import { micromark } from "micromark";
+import { version } from '@/../package.json'
 </script>
 
 <template>
   <h1>{{ $t("mainMenu.aboutAppTitle") }}</h1>
   <div v-html="micromark($t('mainMenu.aboutAppText'))"></div>
+  <div>v{{ version }}</div>
   
 </template>
