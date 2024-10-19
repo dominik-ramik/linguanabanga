b8adc9b3111c2edcc68f2498c45fe4b4b66dd06a
dominik-ramik <26235743+dominik-ramik@users.noreply.github.com>
2024-10-20 09:27:32 +1100

    Fixed bug in protocol check

--- src/utils/normalizeURLPathname.js
+++ src/utils/normalizeURLPathname.js
@@ -1,9 +1,14 @@
 export function normalizeURLPathname(origin, path) {
-    if (path.slice(0, 3).toLowerCase() != "http") {
+    if (path.slice(0, 4).toLowerCase() != "http") {
         path = origin + path
     }
 
-    let pathname = new URL(path).pathname
-
+    let pathname = null
+    try {
+        pathname = new URL(path).pathname
+    }
+    catch (err) {
+        console.error("Error normalizing", origin, path, err)
+    }
     return pathname;
 }
